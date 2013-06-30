fa03bf7c0914712444f0332d7e6541e45e4f8f04
Arjun Singh <arjun810@gmail.com>
2013-06-29 18:41:45 -0700

    Make setup.py cleanup config file.

--- setup.py
+++ setup.py
@@ -4,14 +4,21 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
+class build_ext_with_config(build_ext):
+    def build_extensions(self):
+        print 'Generate config.pxi'
+        filename = os.path.join(os.path.dirname(__file__), 'config.pxi')
+        with open(filename, 'w') as fd:
+                for k, v in c_options.iteritems():
+                            fd.write('DEF %s = %d\n' % (k.upper(), int(v)))
+        build_ext.build_extensions(self)
+        os.remove(filename)
+        cppfilename = os.path.join(os.path.dirname(__file__), 'cyni2.cpp')
+        os.remove(cppfilename)
+
 c_options = { 
         'has_emitter_control': True,
         }
-
-print 'Generate config.pxi'
-with open(os.path.join(os.path.dirname(__file__), 'config.pxi'), 'w') as fd:
-        for k, v in c_options.iteritems():
-                    fd.write('DEF %s = %d\n' % (k.upper(), int(v)))
 
 ext_modules = [
     Extension(
@@ -26,6 +33,6 @@ ext_modules = [
 ]
 setup(
   name = 'cyni2',
-  cmdclass = {'build_ext': build_ext},
+  cmdclass = {'build_ext': build_ext_with_config},
   ext_modules = ext_modules
 )
